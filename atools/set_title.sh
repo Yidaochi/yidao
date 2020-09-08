7729d620753b044551954724ff67a5df3e7d67ff
Yidaochi <chi.swordsman@foxmail.com>
2020-09-08 23:09:05 +0800

    add constrain: only for csh

--- atools/set_title.sh
+++ atools/set_title.sh
@@ -3,7 +3,8 @@
 #  File Name: set_title.sh
 #  Author: Yidaochi
 #  Created Time: Fri 04 Sep 2020 01:15:18 AM CST
+#  only for csh
 #==========================================================================
 
-#add line in your bashrc/cshrc/zshrc: alias set_title="echo -e "\033]0;$1\007"" 
+#add line in your cshrc: alias set_title="echo -e "\033]0;$1\007"" 
 echo -e "\033]0;$1\007"
