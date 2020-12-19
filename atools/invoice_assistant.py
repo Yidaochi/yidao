df58fb2a4d13c58eb465c0bc6ed7e83a470ce49a
Yidaochi <chi.swordsman@foxmail.com>
2020-12-19 13:55:50 +0800

    this script can output a info.txt now

--- atools/invoice_assistant.py
+++ atools/invoice_assistant.py
@@ -2,27 +2,39 @@
 # -*- coding: utf-8 -*-
 """
 @File Name :  invoice_assistant.py
-@Author    :  Liu chi
+@Author    :  Liu Chi
 @Email     :  liuchi@eswin.com
 @Time      :  2020-11-26
 @Desc      :  
 """
 import os
+import time
 
-
-def read_file(path):
+def read_path(path):
   print("[info] path: '%s'" % path)
-  for root, dirs, files in os.walk(path):
-    for f in files:
-      if 'pdf' in f:
-        print(f)
-def pdf_parser(pdf):
-  print("pdf:", pdf)
+  year = time.strftime("%Y")
+  with open('info.txt', 'w') as inf:
+    money_sum = 0
+    date_lst  = []
+    for root, dirs, files in os.walk(path):
+      for f in files:
+        if 'pdf' in f:
+          date  = "%s-%s-%s; " % (year, f.split('-')[0], f.split('-')[1])
+          date_lst.append(date)
+          money = (f.split('-')[-1]).split('.pdf')[0]
+          # print('money:', money)
+          money_sum += float(money)
+    print('money_sum:', money_sum)
+    inf.write("Total %s days.\n" % len(date_lst))
+    for date in date_lst:
+      inf.write(date)
+    inf.write("\nmoney_sum: %s\n" % str(money_sum))
+
+
 
 def main():
   current_path = os.getcwd()
-  read_file(current_path)
-  pdf_parser('.\\21952885.pdf')
+  read_path(current_path)
 
 if __name__ == '__main__':
   main()
